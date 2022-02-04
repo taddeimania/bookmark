3b199728d8adced94d227d6e82350652b8f81af4
Gabriel Aragon <thirtythreebananas@gmail.com>
2022-02-04 11:56:34 -0800

    add form_for and post route

--- lib/bookmark_web/controllers/page_controller.ex
+++ lib/bookmark_web/controllers/page_controller.ex
@@ -11,4 +11,18 @@ defmodule BookmarkWeb.PageController do
       bookmark: Repo.all(from b in Shortcode)
       )
   end
+
+  def create(conn, params) do
+    IO.inspect(params)
+
+    %Shortcode{}
+    |> Shortcode.changeset(%{"title" => params["title"], "url" => params["url"], "code" => params["code"]})
+    |> Repo.insert()
+
+    render(
+      conn,
+      "index.html",
+      bookmark: Repo.all(from b in Shortcode)
+    )
+  end
 end
