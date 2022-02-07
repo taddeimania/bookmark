599eb54800be0180818daa98f64e905e2a1e597f
Gabriel Aragon <thirtythreebananas@gmail.com>
2022-02-07 13:20:32 -0800

    fix gigalixir push

--- lib/bookmark_web/controllers/page_controller.ex
+++ lib/bookmark_web/controllers/page_controller.ex
@@ -3,13 +3,12 @@ defmodule BookmarkWeb.PageController do
   alias Bookmark.{Repo, Shortcode, CodeGenerator}
   import Ecto.Query
 
-
   def index(conn, _params) do
     render(
       conn,
       "index.html",
-      bookmark: Repo.all(from b in Shortcode)
-      )
+      bookmark: Repo.all(from(b in Shortcode))
+    )
   end
 
   def create(conn, params) do
@@ -17,10 +16,10 @@ defmodule BookmarkWeb.PageController do
 
     code = CodeGenerator.generate()
     IO.inspect(code)
+
     %Shortcode{}
     |> Shortcode.changeset(%{"url" => params["url"], "code" => code})
     |> Repo.insert()
-
 
     redirect(conn, to: Routes.page_path(conn, :index))
   end
